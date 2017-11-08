ce8c5e2ba971ae61e8a68e9d084c9c6183e4246e
Zoe <baoliejuan@gmail.com>
2017-11-08 22:50:14 +0800

    modified:   deploy.cpp

--- poj/deploy.cpp
+++ poj/deploy.cpp
@@ -1,12 +1,14 @@
+//POJ1185
 #include <stdio.h>
-int n,m,max[102][1024]={0},high[102]={0},ans=0;
+int n,m,max[102][1024][1024]={0},high[102]={0},ans=0;
+bool check[1024];
 char c;
 int count1(int x,int ans=0){
 	for (;x;x-=x&-x)
 		ans++;
 	return ans;
 }
-bool check(int x){
+bool ck(int x){
 	for (int last=-1;x;x-=x&-x)
 		if (last<0||(x&-x)/last>4)
 			last=x&-x;
@@ -18,8 +20,10 @@ int main(){
 	freopen("input","r",stdin);
 #endif
 	scanf("%d%d",&n,&m);
+	for (int i=0;i<1<<m;i++)
+		check[i]=ck(i);
 	for (int i=0;i<2;i++)
-		for (int j=0;j<1<<m;j++)
+		for (int j=0;j<m;j++)
 			high[i]|=1<<j;
 	for (int i=2;i<n+2;i++)
 		for (int j=0;j<m;j++){
@@ -29,16 +33,16 @@ int main(){
 		}
 	for (int i=2;i<n+2;i++)
 		for (int j=0;j<1<<m;j++)
-			if (!(j&high[i-2]) && check(j))
+			if (check[j]&&!(j&high[i]))
 				for (int k=0;k<1<<m;k++)
-					if (!(k&(j|high[i-1])) && check(k))
+					if (check[k]&&!(k&(j|high[i-1])))
 						for (int l=0;l<1<<m;l++)
-							if (!(l&(j|k|high[i])) && check(l))
-								if (max[i][l]<max[i-2][j]+count1(l)+count1(k))
-									max[i][l]=max[i-2][j]+count1(l)+count1(k);
-	for (int i=0;i<n+2;i++)
+							if (check[l]&&!(l&(j|k|high[i-2])))
+								if (max[i][j][k]<max[i-1][k][l]+count1(j))
+									max[i][j][k]=max[i-1][k][l]+count1(j);
+	for (int i=0;i<1<<m;i++)
 		for (int j=0;j<1<<m;j++)
-			if (ans<max[i][j])
-				ans=max[i][j];
+			if (ans<max[n+1][i][j])
+				ans=max[n+1][i][j];
 	printf("%d\n",ans);
 }
