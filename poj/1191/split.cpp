254192d76ccb5a3caff541e71c90d99b54f1e37a
Zoe <baoliejuan@gmail.com>
2017-11-09 15:09:43 +0800

    modified:   split.cpp

--- poj/1191/split.cpp
+++ poj/1191/split.cpp
@@ -1,2 +1,19 @@
 //POJ1191
 #include <stdio.h>
+int n,a[8][8],sum=0;
+float avr,INF=1e8,dp[8][8][8][8][16]={0};
+float dfs(int x0,int y0,int x1,int y1,int t){
+	float max=0;
+	if ((x1-x0)*(y1-y0)<t)
+		return -INF;
+	for (int i=x0;i
+}
+int main(){
+	scanf("%d",&n);
+	for (int i=0;i<8;i++)
+		for (int j=0;j<8;j++)
+			scanf("%d",&a[i][j]),
+				sum+=a[i][j];
+	avr=float(sum)/64;
+	printf("%.3f\n",dfs(0,0,7,7,n));
+}
